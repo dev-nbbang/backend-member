3a38c581240c0dd7cf69f53a77c9bd31bf1fd3f2
zayson <jymaeng95@gmail.com>
2022-04-08 22:42:47 +0900

    test: 회원 서비스 Jenkins 연동 및 Develop 브랜치 PR시 자동 배포 확인 테스트 - [#3]

--- src/test/java/com/dev/nbbang/member/jenkins/JenkinsTest.java
+++ src/test/java/com/dev/nbbang/member/jenkins/JenkinsTest.java
@@ -8,5 +8,6 @@ public class JenkinsTest {
     @DisplayName("프로젝트 PR시 Webhook 발생 테스트 (Jenkins 자동 빌드 및 배포)")
     void 젠킨스_자동빌드용_테스트() {
         System.out.println("Jenkins Webhook generate auto build Test");
+        System.out.println("PR 시 deploy 라벨 지정");
     }
 }
