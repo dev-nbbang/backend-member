0d886c6877c477787d626768d80256d19351b826
JoonYoung Maeng <jymaeng95@gmail.com>
2022-04-14 15:20:31 +0900

    Revert "feat: 소셜 로그인 구현 (카카오/ 구글) - [#6]"

--- src/main/java/com/dev/nbbang/member/domain/user/repository/OTTViewRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dev.nbbang.member.domain.user.repository;
-
-import com.dev.nbbang.member.domain.user.entity.OTTView;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OTTViewRepository extends JpaRepository<OTTView, Integer> {
-    OTTView findByOttId(int ottId);
-}
