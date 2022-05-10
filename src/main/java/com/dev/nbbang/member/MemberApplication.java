34dd6661310bc88c47d38671fa89a6a45bf24d79
unknown <kimho1995@hs.ac.kr>
2022-05-10 16:33:56 +0900

    fear: eureka setting 설정

--- src/main/java/com/dev/nbbang/member/MemberApplication.java
+++ src/main/java/com/dev/nbbang/member/MemberApplication.java
@@ -3,7 +3,9 @@ package com.dev.nbbang.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MemberApplication {
 	public static void main(String[] args) {
