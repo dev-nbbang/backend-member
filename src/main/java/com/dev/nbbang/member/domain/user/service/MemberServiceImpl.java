12f6f3582b33dfe9a8ce08482564583675f78369
zayson <jymaeng95@gmail.com>
2022-07-05 16:44:20 +0900

    fix: 닉네임 특수문자 및 공백 유효성 검증

--- src/main/java/com/dev/nbbang/member/domain/user/service/MemberServiceImpl.java
+++ src/main/java/com/dev/nbbang/member/domain/user/service/MemberServiceImpl.java
@@ -53,7 +53,7 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public MemberDTO findMemberByNickname(String nickname) {
         // 특수문자 및 공백 제외
-        if(!nickname.matches("[^\\wㄱ-힣]|[\\_]")) {
+        if(!nickname.matches("[^\\wㄱ-힣]|[\\_]") || nickname.length() < 3 || nickname.length() > 10) {
             throw new IllegalNicknameException("옳바르지 않은 닉네임입니다.", NbbangException.ILLEGAL_NICKNAME);
         }
 
