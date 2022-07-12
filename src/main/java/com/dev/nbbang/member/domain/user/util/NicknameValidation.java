649aa4e76af9b78cf02836fbc591fb1c8660f53e
unknown <kimho1995@naver.com>
2022-07-12 15:57:03 +0900

    feat: Pattern 수정

--- src/main/java/com/dev/nbbang/member/domain/user/util/NicknameValidation.java
+++ src/main/java/com/dev/nbbang/member/domain/user/util/NicknameValidation.java
@@ -5,6 +5,6 @@ import java.util.regex.Pattern;
 public class NicknameValidation {
     public static Boolean valid(String nickname) {
         // 특수문자 및 공백 제외
-        return Pattern.compile("[^a-zA-Z0-9ㄱ-힣]").matcher(nickname).find() || nickname.length() < 3 || nickname.length() > 10;
+        return !Pattern.compile("^[0-9|a-z|A-Z|가-힣]*$").matcher(nickname).find() || nickname.length() < 3 || nickname.length() > 10;
     }
 }
