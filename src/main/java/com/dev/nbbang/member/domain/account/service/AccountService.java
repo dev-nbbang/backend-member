a36e57a9fd869f3e3ae50b71a3ec3383cc289140
zayson <jymaeng95@gmail.com>
2022-11-02 23:56:40 +0900

    refactor: 은행 리스트 조회 리팩토링

--- src/main/java/com/dev/nbbang/member/domain/account/service/AccountService.java
+++ src/main/java/com/dev/nbbang/member/domain/account/service/AccountService.java
@@ -1,12 +1,13 @@
 package com.dev.nbbang.member.domain.account.service;
 
+import com.dev.nbbang.member.domain.account.dto.response.BankListResponse;
 import com.dev.nbbang.member.domain.account.entity.Bank;
 
 import java.util.List;
 
 public interface AccountService {
     Bank findByBankId(int bankId);
-    List<Bank> findAll();
+    List<BankListResponse> findAll();
     String encrypt(String text);
     String decrypt(String cipherText);
 }
